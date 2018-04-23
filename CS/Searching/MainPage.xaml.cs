f9c1317a27ad942ee3cc105ed1b7ae6a4a793a79
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Searching/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Xpf.Map;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Searching {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        #region #Search
+        private void bSearch_Click(object sender, RoutedEventArgs e) {
+            searchProvider.Search(tbKeyword.Text, tbLocation.Text);
+        }
+        #endregion #Search
+
+        #region #SearchCompletedEvent
+        private void searchProvider_SearchCompleted(object sender, BingSearchCompletedEventArgs e) {
+            if (e.Cancelled) return;
+
+            StringBuilder sb = new StringBuilder();
+            SearchRequestResult requestResult = e.RequestResult;
+            sb.Append(String.Format("Result Code: {0}\n", requestResult.ResultCode));
+            if (String.IsNullOrEmpty(requestResult.FaultReason))
+                sb.Append(String.Format("Fault Reason: (none)\n", requestResult.FaultReason));
+            else
+                sb.Append(String.Format("Fault Reason: {0}\n", requestResult.FaultReason));
+
+            if (e.RequestResult.ResultCode == RequestResultCode.Success) {
+                sb.Append(String.Format("Search Location: {0}\n", requestResult.Location));
+                sb.Append(String.Format("Search Keyword: {0}\n", requestResult.Keyword));
+                sb.Append(String.Format("Estimated Matches: {0}\n", requestResult.EstimatedMatches));
+                sb.Append(String.Format("SearchRegion: {0}", ProcessLocationInformation(requestResult.SearchRegion)));
+                sb.Append(String.Format("SearchResults:\n{0}", ProcessLocationList(requestResult.SearchResults)));
+                sb.Append(String.Format("Alternate Search Regions:\n{0}", ProcessLocationList(requestResult.AlternateSearchRegions)));
+            }
+            tbResult.Text = sb.ToString();
+        }
+
+        string ProcessLocationList(List<LocationInformation> results) {
+            if (results == null) return "";
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < results.Count; i++) {
+                sb.Append(String.Format("{0}) {1}", i + 1, ProcessLocationInformation(results[i])));
+            }
+            return sb.ToString();
+        }
+        #endregion #SearchCompletedEvent
+
+        #region #ProcessLocationInformation
+        string ProcessLocationInformation(LocationInformation info) {
+            if (info == null) return "";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(String.Format("{0}\n", info.DisplayName));
+            sb.Append(String.Format("\tAdress: {0}\n", info.Address));
+            sb.Append(String.Format("\tLocation: {0}\n", info.Location));
+            return sb.ToString();
+        }
+        #endregion #ProcessLocationInformation
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to  programmatically search using the Bing Map Search service
+
+
+<p>This example demonstrates how to create a custom search panel that searches for location, keywords and other parameters using the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapBingSearchDataProvider_Searchtopic"><u>BingSearchDataProvider.Search</u></a> method.<br />
+</p><p>To start using the Search panel, specify search parameters (location, keyword, start search index, geographical point coordinates) in the textbox elements. <br />
+</p><p>When you handle the <strong>search_Click</strong> event, all parameters are passed to the <strong>Search</strong> method, and you can see the result in the textblock element below. </p><p>The results contain a <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapLocationInformation_DisplayNametopic"><u>display name</u></a>, <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapLocationInformation_EntityTypetopic"><u>entity type</u></a> and  <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapLocationInformation_Addresstopic"><u>address</u></a> associated with the search <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapLocationInformation_Locationtopic"><u>location</u></a>. In addition, the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapSearchRequestResult_AlternateSearchRegionstopic"><u>SearchRequestResult.AlternateSearchRegions</u></a> property returns results of searching alternate regions. <br />
+</p><p>Moreover, you can see search request information returned by the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapRequestResultBase_ResultCodetopic"><u>RequestResultBase.ResultCode</u></a>, <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapRequestResultBase_FaultReasontopic"><u>RequestResultBase.FaultReason</u></a>  and <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapSearchRequestResult_EstimatedMatchestopic"><u>SearchRequestResult.EstimatedMatches</u></a>  properties. <br />
+</p><p>Note that if you run this sample as is, you will get a warning message saying that the specified Bing Maps key is invalid. To learn more about Bing Map keys, please refer to the <a href="http://documentation.devexpress.com/#Silverlight/CustomDocument5975"><u>How to: Get a Bing Maps Key</u></a> tutorial.</p><br />
+
+
+<br/>
+
+
